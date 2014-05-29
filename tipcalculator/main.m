c6385dccf970324b9eea7389fcd334d2304bc904
Gwen Brinsmead <gwenbrin@gmail.com>
2014-05-28 19:31:42 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Guinevere Brinsmead on 5/28/14.
+//  Copyright (c) 2014 gwenbrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
