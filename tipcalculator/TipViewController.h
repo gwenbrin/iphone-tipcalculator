b083d6eb238bf6ea943648881fd99630e6a19134
Gwen Brinsmead <gwen@Gwens-MacBook-Pro.local>
2014-05-31 12:46:02 -0700

    initial

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Guinevere Brinsmead on 5/28/14.
+//  Copyright (c) 2014 gwenbrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
